2724327f79747d5328e9379346e8b9bd74b862a2
Kevin Ross <me@kevinross.name>
2021-03-05 14:47:27 -0500

    Update .appveyor.py

--- .appveyor.py
+++ .appveyor.py
@@ -1,4 +1,10 @@
 from subprocess import check_call, STDOUT
+import glob
+
+# root_dir needs a trailing slash (i.e. /root/dir/)
+for filename in glob.iglob('C:/Program Files (x86)/Microsoft Visual Studio 14.0/**/*.exe', recursive=True):
+     if 'rc.' in filename:
+          print(filename)
 
 check_call('pip install wheel', stderr = STDOUT)
 check_call('python setup.py bdist_wheel', stderr = STDOUT)
